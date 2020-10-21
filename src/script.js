10b25d8821a7dc61774c8a873bd3805b8461b7e0
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-10-22 10:40:30 +1300

    couldnt find typo in JS, used travis's

--- src/script.js
+++ src/script.js
@@ -1,22 +1,22 @@
-const video = document.getElementById('video')
-const play = document.getElementById('play')
-const stop = document.getElementById('stop')
-const progress = document.getElementById('progress')
-const timestamp = document.getElementById('timestamp')
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
 
-// Play & pause Video
+// Play & pause video
 function toggleVideoStatus() {
     if (video.paused) {
-        video.play()
+        video.play();
     } else {
-        video.pause()
+        video.pause();
     }
 }
 
-// update play/pause icon
+// Update play/pause icon
 function updatePlayIcon() {
     if (video.paused) {
-        play.innerHTML = '<i class="fa fa-play fa-2x"></i>'
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
     } else {
         play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
     }
@@ -24,41 +24,40 @@ function updatePlayIcon() {
 
 // Update progress & timestamp
 function updateProgress() {
-    progress.value = (video.currentTime / video.duration) * 100
+    progress.value = (video.currentTime / video.duration) * 100;
 
-    // get minutes
-    let mins = Math.floor(video.currentTime / 60)
+    // Get minutes
+    let mins = Math.floor(video.currentTime / 60);
     if (mins < 10) {
-        mins = '0' + String(mins)
+        mins = '0' + String(mins);
     }
 
-    // get seconds
-    let secs = Math.floor(video.currentTime % 60)
+    // Get seconds
+    let secs = Math.floor(video.currentTime % 60);
     if (secs < 10) {
-        secs = '0' + String(secs)
+        secs = '0' + String(secs);
     }
-    timestamp.innerHTML = `${mins}:${secs}`
+
+    timestamp.innerHTML = `${mins}:${secs}`;
 }
 
-// Set video time to progress 
+// Set video time to progress
 function setVideoProgress() {
-    video.currentTime = (+progress.value * video.duration) / 100
+    video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop video
 function stopVideo() {
-    video.currentTime = 0
-    video.pause()
+    video.currentTime = 0;
+    video.pause();
 }
 
-// Event Listeners
-video.addEventListener('click', toggleVideoStatus)
-video.addEventListener('pause', updatePlayIcon)
-video.addEventListener('play', updatePlayIcon)
-video.addEventListener('timeupdate', updatePlayIcon)
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
 
-video.addEventListener('click', toggleVideoStatus)
-
-video.addEventListener('click', stopVideo)
-
-video.addEventListener('change', setVideoProgress)
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress);
